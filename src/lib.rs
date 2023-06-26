3fa2f6d1022579a1d0abd35b0814ff54af815ea9
Pablo Puyat <pablo@cbrz.com>
2023-06-25 20:18:07 -0400

    Added function to get a feed

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,79 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use reqwest::{header, Client, StatusCode};
+use thiserror::Error;
+
+// Defining our own error type for the OPDS client
+#[derive(Debug, Error)]
+pub enum OpdsClientError {
+    #[error("HTTP request failed")]
+    HttpRequestError(#[from] reqwest::Error),
+    #[error("Unexpected HTTP status code: {0}")]
+    UnexpectedHttpStatus(StatusCode),
+}
+
+pub enum AuthType {
+    Basic(String, String),
+    Bearer(String),
+}
+
+pub struct OpdsClient {
+    client: Client,
+    base_url: String,
+    auth_type: Option<AuthType>,
+}
+
+impl OpdsClient {
+    pub fn new(base_url: String, auth_type: Option<AuthType>) -> Self {
+        let client = Client::new();
+        Self {
+            client,
+            base_url,
+            auth_type,
+        }
+    }
+
+    pub fn request(&self, path: &str) -> reqwest::RequestBuilder {
+        let url = format!("{}{}", self.base_url, path);
+        let request_builder = self.client.get(url);
+        match &self.auth_type {
+            Some(AuthType::Basic(username, password)) => {
+                request_builder.basic_auth(username, Some(password))
+            }
+            Some(AuthType::Bearer(token)) => {
+                request_builder.header(header::AUTHORIZATION, format!("Bearer {}", token))
+            }
+            None => request_builder,
+        }
+    }
+
+    pub async fn fetch_feed(&self, path: &str) -> Result<String, OpdsClientError> {
+        let response = self.request(&path).send().await?;
+
+        if !response.status().is_success() {
+            return Err(OpdsClientError::UnexpectedHttpStatus(response.status()));
+        }
+
+        Ok(response.text().await?)
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use httpmock::prelude::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    #[tokio::test]
+    async fn test_fetch_feed() {
+        let server = MockServer::start();
+
+        let opds_mock = server.mock(|when, then| {
+            when.method(GET).path("/catalog");
+            then.status(200).body("<feed></feed>");
+        });
+        let client = OpdsClient::new(server.base_url(), None);
+
+        let response = client.fetch_feed("/catalog").await;
+        opds_mock.assert();
+        assert!(response.is_ok());
     }
 }
+
